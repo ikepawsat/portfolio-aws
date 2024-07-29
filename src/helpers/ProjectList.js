654c834fb8a169f4cbdb269b62e00f6b6f1f0bd4
ikepawsat <ike.pawsat@gmail.com>
2024-07-28 19:33:34 -0500

    Update ProjectList.js

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -9,11 +9,18 @@ import this_web from "../assets/portfolio_pic.png";
 
 export const ProjectList = [
     {
-        name: "Encryption App",
+        name: "Encryption App (IP)",
         image: encrypt,
-        desc: "Built a desktop app that applied my knowledge of video Encryption and SVD Compression",
-        long_desc: "I wanted to test some of the ideas of SVD Video Compression that I had learned in my Data Science class along with some of the encryption teachings I had received a semester prior.",
+        desc: "*IN PROGRESS* Built a desktop app that applied my knowledge of video Encryption and SVD Compression",
+        long_desc: "Disclaimer, this project is still in development, but I have a working front end so I do not mind showing it. I wanted to test some of the ideas of SVD Video Compression that I had learned in my Data Science class along with some of the encryption teachings I had received a semester prior.",
         github: "https://github.com/ikepawsat/EncryptionApp",
+    },
+    {
+        name: "AI Tag Attempt",
+        image: AITag,
+        desc: "My attempt at an AI Tag game, if you only have a few moments look here please.",
+        long_desc: "This is single handedly the most important project of my career so far. I learned the importance of backups (almost having my entire project deleted), how to create scalable neural networks and save them, how to train AI incrementally, and the patience required with these AI. The improvements that I want to make in the future are training the AI separately and then together. I have to do this project at home with my desktop computer because I run the simulations at high speed and I probably ran over 50 hours of training during this project.",
+        github: "https://github.com/ikepawsat/AI-Tag",
     },
     {
         name: "My First AI",
@@ -21,13 +28,6 @@ export const ProjectList = [
         desc: "This summer I wanted to start advancing my AI skills so I decided to start with this simple AI.",
         long_desc: "Ultimately a failure in my eyes, this gave me a huge foundation to start more advanced projects as it reinforced how to use public information, libraries, and my complex problem solving skills.",
         github: "https://github.com/ikepawsat/Personal-Projects/tree/main/First%20ML%20AI%20Program",
-    },
-    {
-        name: "AI Tag Attempt",
-        image: AITag,
-        desc: "My attempt at an AI Tag game, if you only have a few moments look here please.",
-        long_desc: "This is single handedly the most important project of my career so far. I learned the importance of backups (almost having my entire project deleted), how to create scalable neural networks and save them, how to train AI incrementally, and the patience required with these AI.",
-        github: "https://github.com/ikepawsat/ikepawsat.github.io",
     },
     {
         name: "Football Play Predictor",
@@ -40,8 +40,8 @@ export const ProjectList = [
         name: "This Website",
         image: this_web,
         desc: "Hosted by AWS Amplify, powered with React, Tailwind and GitHub.",
-        long_desc: "I knew I wanted to use AWS Services in one of my projects and I thought what better than to redo my old website and build a new, sleek website that advanced my coding skills. Learning how to setup complex folder interactions along with complex features really helped me push my programming skills. I am really happy with how the features turned out, except that I am not much of a designer.",
-        github: "https://github.com/ikepawsat/portfolio-aws",
+        long_desc: "Firstly, I am choosing to keep this website's code private, if you would like to see/discuss it I am open to it (contact me in the contact page) but I just do not want it public.I knew I wanted to use AWS Services in one of my projects and I thought what better than to redo my old website and build a new, sleek website that advanced my coding skills. Learning how to setup complex folder interactions along with complex features really helped me push my programming skills. I am really happy with how the features turned out, except that I am not much of a designer.",
+        github: "https://github.com/ikepawsat",
     },
     {
         name: "School Projects",
